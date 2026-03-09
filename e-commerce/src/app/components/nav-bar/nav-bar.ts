c7239bc5259f65d9a51978ed6b71355a7ee79a21
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2026-03-09 10:41:25 +0000

    Feat: add navbar with routing

--- e-commerce/src/app/components/nav-bar/nav-bar.ts
+++ e-commerce/src/app/components/nav-bar/nav-bar.ts
@@ -1,8 +1,9 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA] // for allowing elements like el-disclosure
