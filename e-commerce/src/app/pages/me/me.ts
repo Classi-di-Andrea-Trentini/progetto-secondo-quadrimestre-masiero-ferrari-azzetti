b4c20cd5a63f1f12750cbc72c531f1b5d4be10d2
Amedeo Azzetti <amedeo.azzetti@marconirovereto.it>
2026-03-16 16:53:44 +0100

    Feat: inizio struttura del me

--- e-commerce/src/app/pages/me/me.ts
+++ e-commerce/src/app/pages/me/me.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+type TabSection = 'profile' | 'orders' | 'favorites' | 'settings';
 
 @Component({
   selector: 'app-me',
-  imports: [],
+  standalone: true,
+  imports: [NgClass],
   templateUrl: './me.html',
-  styleUrl: './me.css',
+  styleUrls: ['./me.css']
 })
-export class Me {
-
+export class MeComponent {
+  // Gestisce quale scheda è attiva. Parte da 'profile'
+  activeTab = signal<TabSection>('profile');
 }
