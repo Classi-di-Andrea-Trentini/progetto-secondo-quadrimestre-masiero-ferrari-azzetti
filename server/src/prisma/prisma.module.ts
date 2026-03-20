a6f8cc4c875428b74f37ca21f7aae5f8485f01be
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2026-03-20 17:32:13 +0000

    Feat: add api service with first endpoint findAll

--- server/src/prisma/prisma.module.ts
+++ server/src/prisma/prisma.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+@Global()
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports: [PrismaService] // permetto esportazione servizio
 })
 export class PrismaModule {}
