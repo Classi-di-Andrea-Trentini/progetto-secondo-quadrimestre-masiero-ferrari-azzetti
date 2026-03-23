f3f124e7cc5508c0ddfdd8b189321d9604d209aa
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2026-03-23 10:22:01 +0000

    Feat: add first to endpoints to API

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { GetProductsDto } from './dto/get-products.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    public constructor(private readonly productsService: ProductsService) {}
+
+    @Get()
+    products(@Query() dto: GetProductsDto) {
+        return this.productsService.findAll(dto);
+    }
+
+    @Get()
+    product(@Param('id') id: string) {
+        return this.productsService.findOne(id);
+    }
+}
