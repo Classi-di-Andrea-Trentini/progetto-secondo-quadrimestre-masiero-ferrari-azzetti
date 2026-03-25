9e2c44efc4dbb41ec625403d1bd30b964ce85896
Amedeo Azzetti <amedeo.azzetti@marconirovereto.it>
2026-03-25 15:52:10 +0100

    Feat(admin-dashboard): add admin routes and update dashboard layout

--- e-commerce/src/app/app.routes.ts
+++ e-commerce/src/app/app.routes.ts
@@ -6,8 +6,12 @@ import { Products } from './pages/products/products';
 import { MeComponent } from './pages/me/me';
 import { Legal } from './pages/legal/legal';
 import { VerifyEmail } from './pages/verify-email/verify-email';
+import { AdminDashboard } from './pages/admin/admin-dashboard/admin-dashboard';
+import { AdminUsers } from './pages/admin/admin-users/admin-users';
+import { AdminOrders } from './pages/admin/admin-orders/admin-orders';
+import { AdminProducts } from './pages/admin/admin-products/admin-products';
+import { AdminPromo } from './pages/admin/admin-promo/admin-promo';
 import { authGuardGuard } from './guards/auth-guard-guard';
-import { adminGuardGuard } from './guards/admin-guard-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -16,6 +20,12 @@ export const routes: Routes = [
   { path: 'register', component: Register },
   { path: 'products', component: Products },
   { path: 'me', component: MeComponent, canActivate: [authGuardGuard] },
+  { path: 'admin', redirectTo: 'admin-dashboard', pathMatch: 'full' },
+  { path: 'admin-dashboard', component: AdminDashboard },
+  { path: 'admin-users', component: AdminUsers },
+  { path: 'admin-orders', component: AdminOrders },
+  { path: 'admin-products', component: AdminProducts },
+  { path: 'admin-promo', component: AdminPromo },
   { path: 'verify-email/:token', component: VerifyEmail },
   { path: 'legal/:pagina', component: Legal },
   { path: '**', redirectTo: 'home' },
