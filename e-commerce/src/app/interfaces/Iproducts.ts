bc80a9c6b8ec55c2977f22c2fc314ba58ee9db10
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2026-04-01 09:39:54 +0000

    Feat: init interfaces

---
+++ e-commerce/src/app/interfaces/Iproducts.ts
@@ -0,0 +1,70 @@
+export interface Root {
+  data: Daum[]
+  meta: Meta
+}
+
+export interface Daum {
+  id: string
+  categoryId: string
+  name: string
+  slug: string
+  description: string
+  shortDesc: string
+  brand: string
+  basePrice: string
+  isActive: boolean
+  isFeatured: boolean
+  isNewArrival: boolean
+  weightGrams: any
+  metaTitle: any
+  metaDescription: any
+  soldCount: number
+  avgRating: string
+  createdAt: string
+  updatedAt: string
+  category: Category
+  images: Image[]
+  variants: Variant[]
+  discounts: Discount[]
+}
+
+export interface Category {
+  id: string
+  name: string
+  slug: string
+}
+
+export interface Image {
+  id: string
+  productId: string
+  variantId: any
+  url: string
+  altText: string
+  sortOrder: number
+  isCover: boolean
+  width: any
+  height: any
+  createdAt: string
+}
+
+export interface Variant {
+  id: string
+  size: string
+  color: string
+  colorHex: string
+  priceOverride: any
+  stockQty: number
+}
+
+export interface Discount {
+  type: string
+  value: string
+  label: string
+}
+
+export interface Meta {
+  total: number
+  page: number
+  limit: number
+  totalPages: number
+}
