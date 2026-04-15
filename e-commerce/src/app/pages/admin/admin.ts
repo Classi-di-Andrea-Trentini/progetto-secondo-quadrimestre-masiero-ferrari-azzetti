8a49ca55c7c686b84a1e68fba66773cc45c836d5
Alessio Ferrari <alessio.ferrari@marconirovereto.it>
2026-04-15 09:26:39 +0000

    Feat: implement admin dashboard and routing

--- e-commerce/src/app/pages/admin/admin.ts
+++ e-commerce/src/app/pages/admin/admin.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { AuthService } from '../../services/auth';
 
 @Component({
   selector: 'app-admin',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './admin.css',
 })
 export class Admin {
+  // faccio inject servizio auth
+  private auth = inject(AuthService);
+  // utente corrente
+  private _currentUser = this.auth.currentUser;
 
+  // signal che controlla se è admin
+  readonly isAdmin = computed(() => this._currentUser()?.role === 'admin');
 }
